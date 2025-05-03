88f279708a63d9aa4b0bbdcae308d6be5b1b2be0
orroo <omarriahi2003@gmail.com>
2025-05-03 20:53:06 +0100

    template

---
+++ project/duration_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def about(request):
+    return render(request,"about-us.html")
+
+
+def blog(request):
+    return render(request,"blog.html")
+
+
+def contact(request):
+    return render(request,"contact.html")
+
+
+def department(request):
+    return render(request,"department.html")
+
+    
+def doctors(request):
+    return render(request,"doctors.html")
+
+
+    
+def element(request):
+    return render(request,"element.html")
+
+
+    
+def index(request):
+    return render(request,"index.html")
+
+
+    
+def singleblog(request):
+    return render(request,"single-blog.html")
+
